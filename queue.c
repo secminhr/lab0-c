8474de3c949221f341ddde26a98270d8f3239ab9
secminhr <secminhrian@gmail.com>
2021-02-24 20:19:27 +0800

    Implement q_new
    Handle the case if malloc returned NULL and initialize all fields.

--- queue.c
+++ queue.c
@@ -12,8 +12,10 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != NULL) {
+        q->head = NULL;
+        q->size = 0;
+    }
     return q;
 }
 
